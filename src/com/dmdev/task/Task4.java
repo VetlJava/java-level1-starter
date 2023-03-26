b90d390e9b9e1b990e8a9e835f8559aaef082527
VetlJava <vetl.foto@gmail.com>
2023-03-26 14:18:07 +0300

    task Task4 - Високосный год

---
+++ src/com/dmdev/task/Task4.java
@@ -0,0 +1,29 @@
+package com.dmdev.task;
+
+/**
+ * 4. Написать программу, которая считывает год, введённый пользователем, и определяет,
+ *    является ли этот год високосным.
+ *    Год считается високосным, если он делится без остатка на 4. Однако, если год также делится на 100,
+ *    то этот год не високосный, за исключением годов, делящихся на 400.
+ *    Например:
+ *    1992 - високосный
+ *    1900 - не високосный
+ *    2000 – високосный
+ */
+public class Task4 {
+    public static void main(String[] args) {
+        int value = 2000;
+        isLeepYear(value);
+    }
+
+    public static boolean isLeepYear(int year) {
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+//        {
+//            return true;
+////            System.out.println(year + " год високосный");
+//        } else {
+//            return false;
+////            System.out.println(year + " год не високосный");
+//        }
+    }
+}
