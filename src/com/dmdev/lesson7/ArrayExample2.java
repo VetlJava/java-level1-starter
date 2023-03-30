695aa054a40c242e7e69d12321bc09bee559bdb2
VetlJava <vetl.foto@gmail.com>
2023-03-30 11:15:50 +0300

    Массивы - трёхмерные

--- src/com/dmdev/lesson7/ArrayExample2.java
+++ src/com/dmdev/lesson7/ArrayExample2.java
@@ -2,6 +2,15 @@ package com.dmdev.lesson7;
 
 public class ArrayExample2 {
     public static void main(String[] args) {
+        int[][] values = new int[3][];  //3 - размер первого измерения
+        values[0] = new int[5];
+        values[1] = new int[2];
+        values[2] = new int[1];
+        printArray(values);
+
+    }
+
+    private static void test() {
         int[] values = {1, 2, 3, 4, 5};
 //        двумерный массив
         int[][] values2 = {
@@ -9,6 +18,11 @@ public class ArrayExample2 {
                 {4, 5, 6},
                 {7, 8, 9}
         };
+
+        printArray(values2);
+    }
+
+    private static void printArray(int[][] values2) {
         for (int i = 0; i < values2.length; i++) {
             int[] ints = values2[i];
             for (int j = 0; j < ints.length; j++) {
