943cb3e4e4a795ba12b67b5553f159ad84dc782c
VetlJava <vetl.foto@gmail.com>
2023-03-30 15:21:59 +0300

    практика. Task1 - Foreach

--- src/com/dmdev/lesson7/task/Task1.java
+++ src/com/dmdev/lesson7/task/Task1.java
@@ -7,10 +7,19 @@ public class Task1 {
     public static void main(String[] args) {
         int[] values = {1, 5, 7, 9, 11};
         printArray(values);
+        printArrayForeach(values);
     }
     public static void  printArray(int[] values){
         for (int i = 0; i < values.length; i++) {
-            System.out.print(values[i]);
+            System.out.print(values[i] + " ");
+        }
+        System.out.println();
+    }
+
+//      аналогичен предыдущему методу
+    public static void  printArrayForeach(int[] values) {
+        for (int value : values) {
+            System.out.print(value + " ");
         }
         System.out.println();
     }
